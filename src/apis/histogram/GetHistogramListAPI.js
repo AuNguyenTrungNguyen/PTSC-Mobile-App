fe2bf2d6bb0dbbb135ea714b80631ec4ccb53c13
Admin <Admin@MacBook-Pro.local>
2020-12-11 08:06:02 +0700

    Fix API Histogram

--- src/apis/histogram/GetHistogramListAPI.js
+++ src/apis/histogram/GetHistogramListAPI.js
@@ -1,6 +1,6 @@
 import { Port_Server } from '../../utils/Core';
 const GetHistogramListAPI = ((projectCode, token) =>
-  fetch(Port_Server + '/api/Drawing/GetHistogramList?projectCode=' + projectCode, {
+  fetch(Port_Server + '/api/Reports/GetHistogramList?projectCode=' + projectCode, {
     headers: {
       'Authorization': 'Bearer ' + token,
       'Content-Type': 'application/json',
