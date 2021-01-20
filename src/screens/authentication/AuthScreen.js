eb9cf7483fa7e60bbcbf5601ab514024636bc835
Trung Nguyên <nguyen06111996@gmail.com>
2021-01-20 07:21:09 +0700

    Add Check old version and force update

--- src/screens/authentication/AuthScreen.js
+++ src/screens/authentication/AuthScreen.js
@@ -1,26 +1,62 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Alert, Platform, Linking } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import Moment from 'moment';
+import DeviceInfo from 'react-native-device-info';
+import RNExitApp from 'react-native-exit-app';
 
 import Helper from '../../utils/Helper';
 
+const ANDROID_STORE = 'https://play.google.com/store/apps/details?id=com.ptsc_cloud';
+const IOS_STORE = 'https://testflight.apple.com/join/4EoDbXUS';
+const LATEST_VERSION = '2.0';
+const LATEST_BUILD_NUMBER = '18';
+
 export default ({ navigation }) => {
+
+  const version = DeviceInfo.getVersion();
+  const buildNumber = DeviceInfo.getBuildNumber();
 
   useEffect(() => {
     const getRoute = async () => {
-      let expires = await Helper.getData('EXPIRES');
-      if (expires && (Moment.utc(new Date(expires)).valueOf() - Moment.utc(new Date()).valueOf() > 0)) {
-        let projectCode = await Helper.getData('PROJECT_CODE');
-        let disciplineCode = await Helper.getData('DISCIPLINE_CODE');
-        navigation.replace('Home', { projectCode: projectCode, disciplineCode: disciplineCode });
+      if (version === LATEST_VERSION && buildNumber === LATEST_BUILD_NUMBER) {
+        let expires = await Helper.getData('EXPIRES');
+        if (expires && (Moment.utc(new Date(expires)).valueOf() - Moment.utc(new Date()).valueOf() > 0)) {
+          let projectCode = await Helper.getData('PROJECT_CODE');
+          let disciplineCode = await Helper.getData('DISCIPLINE_CODE');
+          navigation.replace('Home', { projectCode: projectCode, disciplineCode: disciplineCode });
+        } else {
+          navigation.replace('Login');
+        }
+        SplashScreen.hide();
       } else {
-        navigation.replace('Login');
+        Helper.clearData();
+        SplashScreen.hide();
+        Alert.alert(
+          'Update New Version',
+          'Please update the new version to continue using the app.',
+          [
+            {
+              text: 'Update and Reopen',
+              onPress: updateNewVersion,
+            },
+          ],
+          { cancelable: false },
+        );
       }
-      SplashScreen.hide();
     };
     getRoute();
   }, []);
+
+  const updateNewVersion = async () => {
+    if (Platform.OS === 'android') {
+      await Linking.openURL(ANDROID_STORE);
+      RNExitApp.exitApp();
+    } else {
+      await Linking.openURL(IOS_STORE);
+      RNExitApp.exitApp();
+    }
+  };
 
   return (
     <SafeAreaView />
