2c93d889baa31ef376d7779bab7d4a59361c92b7
AuNguyenTrungNguyen <nguyen06111996@gmail.com>
2022-05-23 15:02:52 +0700

    [Fix] Fix select date

--- src/screens/piping/drawing/DrawingDetailScreen.js
+++ src/screens/piping/drawing/DrawingDetailScreen.js
@@ -427,7 +427,7 @@ const DrawingDetailScreen = ({ route, navigation }) => {
     setIndexUpdate(index);
     setKeyUpdate(key);
     if (value) {
-      setDateDisplay(value);
+      setDateDisplay(new Date(Moment(value).format("YYYY-MM-DDT00:00:00")));
     } else {
       setDateDisplay(new Date());
     }
